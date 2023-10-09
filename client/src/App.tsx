b267e52b0f1ed434945957e3895baa9d99e0079d
Babe <panyawit.sea@gmail.com>
2023-10-10 01:41:41 +0700

    feat:add landing page

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import AuthenticatedApp from "./pages/AuthenticatedApp";
+
+function App() {
+  return (
+    <>
+      <AuthenticatedApp />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/AuthenticatedApp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AuthenticatedApp() {
+  return <div className=" bg-black text-red-700">AuthenticatedApp</div>;
+}
+
+export default AuthenticatedApp;
